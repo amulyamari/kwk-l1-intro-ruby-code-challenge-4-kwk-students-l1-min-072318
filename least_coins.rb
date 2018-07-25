31b3b44ce9cb4a7a17f329e7be1c0ebfeacaa7ac
amulyamari <amulya@saimail.com>
2018-07-25 20:58:41 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,6 +1,14 @@
 def least_coins(coins)
-quarters+=1
-  
+  coins={"quarters"=> "0",
+  "nickels"=>"0",
+  "dimes"=>"0",
+  "pennies"=>"0"
 }
+    quarters= cents/25
+      if quarters==0
+        coins [quarters]=0
+      else quarter != 0
+        coins [quarters]= quarters
+        cents = cents-quarters *25
 end
 least_coins(29)
